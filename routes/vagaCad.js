8740481513e967cd1247a638cce6c8e8a5df3f2a
Júlio César Bomfim Ferreira <betobetuelferreira@gmail.com>
2022-11-21 11:50:00 -0300

    alterar

---
+++ routes/vagaCad.js
@@ -0,0 +1,6 @@
+/* module.exports = 
+    function(app) {
+        let vaga = app.controllers.vagaCad;
+        app.get('/vagaCad', vaga.vagaCad);
+        app.post('/vagaCad', vaga.vagaCade);     
+} */
