e94fe6145aa79c6e559cace423986ba15df5307b
Betuelferreira <betobetuelferreira@gmail.com>
2022-12-04 22:12:14 -0300

    .

--- routes/vagaCad.js
+++ routes/vagaCad.js
@@ -1,9 +1,14 @@
 module.exports =
     function(app) {
-        let vaga = app.controllers.vagaCad;
-        app.get('/vagaCad', vaga.vagaCad);
-        /*  let autenticar = require('../middlewares/autenticador'); */
-        /* app.post('/vagaCade', vaga.vagaCade); */
+        let vagac = app.controllers.vagaCad;
+        app.get('/vagaCad', vagac.vagaCad);
+        /*  let autenticar = require('../middlewares/autenticador');
+        app.get("/vagaCad", autenticar, vagac.vagaCad);
+        app.get("/vagaCad/:id", autenticar, vagac.show);
+        app.post("/vagaCad", autenticar, vagac.create);
+        app.get("/vagaCad/:id/editar", vagac.edit);
+        app.put("/vagaCad/:id", autenticar, vagac.update);
+        app.delete("/vagaCad/:id", autenticar, vagac.destroy);
 
-
+ */
     }
