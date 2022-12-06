6811d191cf74ce433764249289a62dfc25a12e06
Júlio César Bomfim Ferreira <betobetuelferreira@gmail.com>
2022-12-06 08:26:31 -0300

    autenticação

--- routes/vagaCad.js
+++ routes/vagaCad.js
@@ -1,14 +1,7 @@
 module.exports =
     function(app) {
         let vagac = app.controllers.vagaCad;
-        app.get('/vagaCad', vagac.vagaCad);
-        /*  let autenticar = require('../middlewares/autenticador');
-        app.get("/vagaCad", autenticar, vagac.vagaCad);
-        app.get("/vagaCad/:id", autenticar, vagac.show);
-        app.post("/vagaCad", autenticar, vagac.create);
-        app.get("/vagaCad/:id/editar", vagac.edit);
-        app.put("/vagaCad/:id", autenticar, vagac.update);
-        app.delete("/vagaCad/:id", autenticar, vagac.destroy);
+        let autenticar = require('../middlewares/autenticador');
 
- */
+        app.get('/vagaCad', autenticar,vagac.vagaCad);
     }
