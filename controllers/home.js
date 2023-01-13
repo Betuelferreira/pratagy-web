3bee9b59deefb5ab7a0d218f00a4f50e9f893149
Betuelferreira <betobetuelferreira@gmail.com>
2023-01-12 23:14:58 -0300

    exibir

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,11 @@
 module.exports=function(app){
     var HomeController = {
         index:function (req, res) {
-           /*  let usuario = req.session.usuario;
-            let params = { usuario: usuario, vagas: usuario.vagas }; */ 
-            res.render("home/index", /* params */);
+            
+            let usuario = req.session.usuario;
+            /* let usuario = "teste" */
+            let params = { usuario: usuario };
+            res.render("home/index", params );
     },
     
     };
